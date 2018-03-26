fd8bc8197170f8f54038aa878c90e442f1dac4cb
wangyv <1710670484@qq.com>
2018-03-26 22:26:18 +0800

    入口的路由配置

---
+++ application/controllers/Welcome.php
@@ -0,0 +1,18 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Welcome extends CI_Controller {
+	public function index()
+	{
+		$this->load->view('index');
+	}
+	public function login()
+	{
+		$this->load->view('login');
+	}
+	public function register()
+	{
+		$this->load->view('reg');
+	}
+	
+}
