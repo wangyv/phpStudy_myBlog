a88d6cfca8fcf93a8bc998f6b7eef6b5cf3fb796
wangyv <1710670484@qq.com>
2018-03-29 22:03:34 +0800

    博客管理

---
+++ application/views/blog_management.php
@@ -0,0 +1,61 @@
+<?php
+include 'admin_check.php';
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xml:lang="zh-CN" xmlns="http://www.w3.org/1999/xhtml" lang="zh-CN"><head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <meta http-equiv="Content-Language" content="zh-CN">
+  <title>博客文章管理 Johnny的博客 - 唯创个人博客</title>
+  <base href="<?php echo site_url()?>">
+      <link rel="stylesheet" href="css/space2011.css" type="text/css" media="screen">
+  <link rel="stylesheet" type="text/css" href="css/jquery.css" media="screen">
+    body,table,input,textarea,select {font-family:Verdana,sans-serif,宋体;}	
+  </style>
+</head>
+<body>
+<!--[if IE 8]>
+<style>ul.tabnav {padding: 3px 10px 3px 10px;}</style>
+<![endif]-->
+<!--[if IE 9]>
+<style>ul.tabnav {padding: 3px 10px 4px 10px;}</style>
+<![endif]-->
+<div id="OSC_Screen">
+<?php include 'admin_header.php'?>
+<div id="OSC_Content">
+<div id="AdminScreen">
+    <div id="AdminPath">
+        <a href="index_logined.htm">返回我的首页</a>&nbsp;»
+    	<span id="AdminTitle">博客文章管理</span>
+    </div>
+    <?php include 'admin_menu.php'?>
+    <div id="AdminContent">
+<div class="MainForm BlogArticleManage">
+  <h3 class="title">共有 3 篇博客，每页显示 40 个，共 1 页</h3>
+    <div id="BlogOpts">
+	<a href="javascript:;" onclick="select_all();">全选</a>&nbsp;|
+	<a href="javascript:;" onclick="unselect_all();">取消</a>&nbsp;|
+	<a href="javascript:;" onclick="select_other();">反向选择</a>&nbsp;|
+	<a href="javascript:;" onclick="delete_sel()">删除选中</a>
+  </div>
+  <ul>
+		<li class="row_1">
+		<input name="blog" value="24027" type="checkbox">
+		<a href="viewPost_comment.htm" target="_blank">测试文章3</a>
+		<small>2011-06-18 00:34</small>
+	</li>
+		<li class="row_0">
+		<input name="blog" value="24026" type="checkbox">
+		<a href="viewPost_logined.htm" target="_blank">测试文章2</a>
+		<small>2011-06-17 23:06</small>
+	</li>
+		<li class="row_1">
+		<input name="blog" value="24025" type="checkbox">
+		<a href="viewPost.htm" target="_blank">测试文章1</a>
+		<small>2011-06-17 23:04</small>
+	</li>
+	  </ul>
+    </div>
+</div>
+	<div class="clear"></div>
+</div>
+</body></html>
