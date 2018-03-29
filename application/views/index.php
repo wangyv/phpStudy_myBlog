b0d6f2a79887ed9f6bbe09a3371e3baa131d5d4d
wangyv <1710670484@qq.com>
2018-03-29 22:04:01 +0800

    游客首页

---
+++ application/views/index.php
@@ -0,0 +1,109 @@
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xml:lang="zh-CN" xmlns="http://www.w3.org/1999/xhtml" lang="zh-CN"><head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <meta http-equiv="Content-Language" content="zh-CN">
+  <title>Johnny的博客 - 唯创个人博客</title>
+  <base href="<?php echo site_url()?>">
+      <link rel="stylesheet" href="css/space2011.css" type="text/css" media="screen">
+  <link rel="stylesheet" type="text/css" href="css/jquery.css" media="screen">
+  <script type="text/javascript" src="js/jquery-1.js"></script>
+  <script type="text/javascript" src="js/jquery.js"></script>
+  <script type="text/javascript" src="js/jquery_002.js"></script>
+  <script type="text/javascript" src="js/oschina.js"></script>
+  <style type="text/css">
+    body,table,input,textarea,select {font-family:Verdana,sans-serif,宋体;}	
+  </style>
+</head>
+<body>
+<!--[if IE 8]>
+<style>ul.tabnav {padding: 3px 10px 3px 10px;}</style>
+<![endif]-->
+<!--[if IE 9]>
+<style>ul.tabnav {padding: 3px 10px 4px 10px;}</style>
+<![endif]-->
+<div id="OSC_Screen"><!-- #BeginLibraryItem "/Library/OSC_Banner.lbi" -->
+<div id="OSC_Banner">
+    <div id="OSC_Slogon">Johnny's Blog</div>
+    <div id="OSC_Channels">
+        <ul>
+        <li><a href="#" class="project">心情 here...</a></li>
+        </ul>
+    </div>
+    <div class="clear"></div>
+</div><!-- #EndLibraryItem --><div id="OSC_Topbar">
+	  <div id="VisitorInfo">
+		当前访客身份：
+				游客 [ <a href="welcome/login">登录</a> | <a href="welcome/register">注册</a> ]
+  </div>
+		<div id="SearchBar">
+    		<form action="Search">
+								<input name="user" value="154693" type="hidden">
+																								<input id="txt_q" name="q" class="SERACH" value="在此空间的博客中搜索" onblur="(this.value=='')?this.value='在此空间的博客中搜索':this.value" onfocus="if(this.value=='在此空间的博客中搜索'){this.value='';};this.select();" type="text">
+				<input class="SUBMIT" value="搜索" type="submit">
+    		</form>
+		</div>
+		<div class="clear"></div>
+	</div>
+	<div id="OSC_Content">
+	<div class="clear"></div>
+<div class="BlogList">
+<ul>
+  <li class='Blog' id='blog_24027'>
+
+	<h2 class='BlogAccess_true BlogTop_0'><a href="viewPost_comment.htm">测试文章3</a></h2>
+
+	<div class='outline'>
+
+	  <span class='time'>发表于 2011年06月18日 0:34</span>
+
+	  <span class='catalog'>分类: <a href="?catalog=92334">工作日志</a></span>
+
+	  <span class='stat'>统计: 0评/0阅</span>
+
+	  	</div>
+
+		<div class='TextContent' id='blog_content_24027'>
+
+				测试文章3
+
+		<div class='fullcontent'><a href="viewPost_comment.htm">阅读全文...</a></div>
+
+			</div>
+
+	  </li>
+  <li class="Blog" id="blog_24026">
+	<h2 class="BlogAccess_true BlogTop_0"><a href="viewPost_logined.htm">测试文章2</a></h2>
+	<div class="outline">
+	  <span class="time">发表于 2011年06月17日 23:06</span>
+	  <span class="catalog">分类: <a href="#">工作日志</a></span>
+	  <span class="stat">统计: 0评/1阅</span>
+	  	</div>
+		<div class="TextContent" id="blog_content_24026">
+				测试文章1
+		<div class="fullcontent"><a href="viewPost.htm">阅读全文...</a></div>
+			</div>
+	  </li>
+  <li class="Blog" id="blog_24025">
+	<h2 class="BlogAccess_true BlogTop_0"><a href="viewPost.htm">测试文章1</a></h2>
+	<div class="outline">
+	  <span class="time">发表于 2011年06月17日 23:04</span>
+	  <span class="catalog">分类: <a href="#">工作日志</a></span>
+	  <span class="stat">统计: 0评/3阅</span>
+	  	</div>
+		<div class="TextContent" id="blog_content_24025">
+				<b>测试文章1</b>
+		<div class="fullcontent"><a href="viewPost.htm">阅读全文...</a></div>
+			</div>
+	  </li>
+</ul>
+<div class="clear"></div>
+	</div>
+<div class="clear"></div>
+<script type="text/javascript" src="js/brush.js"></script>
+<link type="text/css" rel="stylesheet" href="css/shCore.css">
+<link type="text/css" rel="stylesheet" href="css/shThemeDefault.css"></div>
+	<div class="clear"></div>
+	<div id="OSC_Footer">© 唯创网讯</div>
+</div>
+</body></html>
