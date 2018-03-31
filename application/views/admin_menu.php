455493c3e5aa9b9bfbdd2db81c1aa3667d97f0ee
wangyv <1710670484@qq.com>
2018-03-31 21:09:44 +0800

    公共界面的菜单栏

--- application/views/admin_menu.php
+++ application/views/admin_menu.php
@@ -2,10 +2,10 @@
 		<ul>
 			<li class="caption">个人信息管理		
 				<ol>
-					<li><a href="inbox.htm">站内留言(0/1)</a></li>
-					<li><a href="profile.htm">编辑个人资料</a></li>
-					<li><a href="chpwd.htm">修改登录密码</a></li>
-					<li><a href="userSettings.htm">网页个性设置</a></li>
+					<li class="inbox"><a href="admin/inbox">站内留言(0/1)</a></li>
+					<li class="profile"><a href="admin/profile">编辑个人资料</a></li>
+					<li class="chpwd"><a href="admin/chpwd">修改登录密码</a></li>
+					<li class="usersettings"><a href="admin/usersettings">网页个性设置</a></li>
 				</ol>
 			</li>		
 		</ul>
@@ -13,9 +13,9 @@
 			<li class="caption">博客管理	
 				<ol>
 					<li class='new_blog'><a href="admin/new_blog">发表博客</a></li>
-					<li><a href="blogCatalogs.htm">博客设置/分类管理</a></li>
-					<li><a href="blogs.htm">文章管理</a></li>
-					<li class='blog_manage'><a href="admin/blog_manage">博客评论管理</a></li>
+					<li class='classification'><a href="admin/classification">博客设置/分类管理</a></li>
+					<li class='blog_manage'><a href="admin/blog_manage">文章管理</a></li>
+					<li class='blogcomments'><a href="admin/blogcomments">博客评论管理</a></li>
 				</ol>
 			</li>
 		</ul>
@@ -24,5 +24,12 @@
 <script src="js/jquery.min.js"></script>
 <script>
     var str = location.href.split('/').pop();
+	var substr = str.split('?')[0];
+	console.log(substr);
+	if(str == 'outbox'){
+		str = 'inbox';
+	}else if(substr == 'change_blog'){
+		str = 'new_blog';
+	}
     $('#AdminMenu .' + str).addClass('current');
 </script>
