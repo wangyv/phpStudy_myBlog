7651130dda6d159d82f27b2ded5cf5ad74706911
wangyv <1710670484@qq.com>
2018-04-01 21:10:35 +0800

    增加异步操作，判断是否可以修改密码

--- application/views/chpwd.php
+++ application/views/chpwd.php
@@ -2,6 +2,11 @@
 include 'admin_check.php';
 ?>
 
+
+
+
+
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xml:lang="zh-CN" xmlns="http://www.w3.org/1999/xhtml" lang="zh-CN"><head>
   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
@@ -12,6 +17,7 @@ include 'admin_check.php';
   <link rel="stylesheet" type="text/css" href="css/jquery.css" media="screen">
   <style type="text/css">
     body,table,input,textarea,select {font-family:Verdana,sans-serif,宋体;}	
+	.error-pwd{color:red;}
   </style>
 </head>
 <body>
@@ -33,13 +39,13 @@ include 'admin_check.php';
     <div id="AdminContent">
 
 <div class="MainForm">
-<form class="AutoCommitJSONForm" action="/action/profile/passwd" method="POST">
+<form class="AutoCommitJSONForm" action="" method="POST">
 <h2>修改我的登录密码</h2>
 <table width="100%">
 	<tbody><tr>
 		<th width="110">旧的登录密码</th>		
 		<td>
-			<input name="pwd" size="20" class="TEXT" tabindex="1" type="password">&nbsp;&nbsp;&nbsp;&nbsp;		
+			<input name="pwd" size="20" class="TEXT" tabindex="1" type="password" id="old-pwd">&nbsp;&nbsp;&nbsp;&nbsp;		
 			<a href="#" target="_blank">忘记登录密码</a>
 		</td>    		
 	</tr>
@@ -49,13 +55,13 @@ include 'admin_check.php';
 	</tr>
 	<tr>
 		<th>再次输入新密码</th>		
-		<td><input name="newpwd2" size="20" class="TEXT" tabindex="3" type="password"></td>    		
+		<td><input name="newpwd2" size="20" class="TEXT" tabindex="3" type="password">&nbsp;&nbsp;&nbsp;<small class="error-pwd"></small></td>    		
 	</tr>
 	<tr><th colspan="2"></th></tr>
 	<tr class="submit">
 		<th></th>
 		<td>
-		<input value="修改密码" class="BUTTON SUBMIT" tabindex="4" type="submit">
+		<input value="修改密码" class="BUTTON SUBMIT" tabindex="4" type="button" id="change-btn">
 		<span id="error_msg" style="display:none"></span>
 		</td>
 	</tr>
@@ -68,5 +74,33 @@ include 'admin_check.php';
 	<div class="clear"></div>
 	<div id="OSC_Footer">© 唯创网讯</div>
 </div>
+<script src="js/jquery.min.js"></script>
+<script>
+	$('[name=newpwd2]').on('keyup',function(){
+		var pwd = $('[name=newpwd]').val();
+		var pwd2 = $('[name=newpwd2]').val();
+		if(pwd2 != pwd){
+			$('.error-pwd').html('两次请输入相同密码');
+		}else{
+			$('.error-pwd').html('');
+		}
+	}).on('blur',function(){
+		$('.error-pwd').html('');
+	})
+	$('#change-btn').on('click',function(){
+		$.post('user/check_old_pwd', {
+			'old_pwd':$('[name=pwd]').val(),
+			'new_pwd':$('[name=newpwd]').val()
+		}, function(res){
+			if(res == 'fail_oldpwd'){
+				alert('错误的旧密码！');
+			}else if(res == 'success'){
+				alert('密码修改成功！');
+			}else if(res == 'fail_change'){
+				alert('密码修改失败！');
+			}
+		}, 'text')
+	})
 
+</script>
 </body></html>
