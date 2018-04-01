9fa163615dd0b517cdd10d908e7e9d9c53f3e776
wangyv <1710670484@qq.com>
2018-04-01 21:08:40 +0800

    操作uesr表，更新一条数据

--- application/models/User_model.php
+++ application/models/User_model.php
@@ -22,4 +22,16 @@ class User_model extends CI_Model {
             'email' => $email
         )) -> row();
     }
+    public function update_by_email($email, $users){
+        $this -> db -> where('email',$email);
+        $this -> db -> update('t_user', $users);
+        return $this -> db -> affected_rows();
+    }
+    public function update_pwd_by_email($new_pwd, $email){
+        $this -> db -> where('email',$email);
+        $this -> db -> update('t_user', array(
+            'password' => $new_pwd
+        ));
+        return $this -> db -> affected_rows();
+    }
 }
