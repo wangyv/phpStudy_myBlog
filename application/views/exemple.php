d2dbcd3f6e8984a913d5d48031e0469fe216dc5a
wangyv <1710670484@qq.com>
2018-04-02 21:39:51 +0800

    模板

---
+++ application/views/exemple.php
@@ -0,0 +1,12 @@
+<?php
+include 'admin_check.php';
+?>
+  
+  <?php echo $user -> username?>
+<base href="<?php echo site_url()?>">
+
+<?php include 'admin_header.php'?>
+
+admin/index
+
+<?php include 'admin_menu.php'?>
