815a05cca7b2ec9c8f9dd59bb6b9bc25dd6be9ec
wangyv <1710670484@qq.com>
2018-04-07 22:21:25 +0800

    增加异步的修改，增加，删除功能

--- application/views/editCatalog.php
+++ application/views/editCatalog.php
@@ -9,6 +9,7 @@ include 'admin_check.php';
   <base href="<?php echo site_url()?>">
       <link rel="stylesheet" href="css/space2011.css" type="text/css" media="screen">
   <link rel="stylesheet" type="text/css" href="css/jquery.css" media="screen">
+  <style>
     body,table,input,textarea,select {font-family:Verdana,sans-serif,宋体;}	
   </style>
 </head>
@@ -32,15 +33,14 @@ include 'admin_check.php';
     <?php include 'admin_menu.php'?>
     <div id="AdminContent">
 <div class="MainForm BlogCatalogManage">
-<form id="CatalogForm" action="/action/blog/add_blog_catalog?space=154693&amp;id=92334" method="post">
+<form id="CatalogForm" action="" method="post">
     <h3> 修改博客分类 </h3>
     <div id="error_msg" class="error_msg" style="display:none;"></div>
-    <label>分类名称:</label><input id="txt_link_name" name="name" value="工作日志" size="15" tabindex="1" type="text">
-    <label>排序值:</label><input name="sort_order" value="1" size="3" type="text">
+    <label>分类名称:</label><input id="txt_link_name" name="name" size="15" tabindex="1" type="text">
     <span class="submit">
-          <input value="修改&nbsp;»" tabindex="3" class="BUTTON SUBMIT" type="submit">
-      <input value="取消" class="BUTTON" onclick="location.href='blogCatalogs.htm';" type="button">
-        </span>
+        <input value="添加 »" id="toggle-change" tabindex="3" class="BUTTON SUBMIT" type="button">
+      	<input value="取消" id="cansel-change" class="BUTTON" type="button" style="display:none">
+    </span>
 </form>
 <form class="BlogCatalogs">
 <h3>博客分类 <span>(点击分类名编辑)</span></h3>
@@ -51,33 +51,22 @@ include 'admin_check.php';
 		<th>文章</th>
 		<th>操作</th>
 	</tr>
-	<tr id="catalog_92334">
-		<td class="idx">1</td>
-		<td class="name"><a href="editCatalog.htm" title="点击修改博客分类">工作日志</a></td>
-		<td class="num">3</td>
-		<td class="opts">
-			<a href="editCatalog.htm" title="点击修改博客分类">修改</a>
-			<a href="#" onclick="return delete_catalog(154693,92334);">删除</a>
-		</td>
-	</tr>
-	<tr id="catalog_92335">
-		<td class="idx">2</td>
-		<td class="name"><a href="#" title="点击修改博客分类">日常记录</a></td>
-		<td class="num">0</td>
-		<td class="opts">
-			<a href="#" title="点击修改博客分类">修改</a>
-			<a href="#" onclick="return delete_catalog(154693,92335);">删除</a>
-		</td>
-	</tr>
-	<tr id="catalog_92336">
-		<td class="idx">3</td>
-		<td class="name"><a href="#" title="点击修改博客分类">转贴的文章</a></td>
-		<td class="num">0</td>
-		<td class="opts">
-			<a href="#" title="点击修改博客分类">修改</a>
-			<a href="#" onclick="return delete_catalog(154693,92336);">删除</a>
-		</td>
-	</tr>
+
+	<?php 
+		foreach($results as $key => $result){
+	?>
+		<tr id="catalog_92334">
+			<td class="idx"><?php echo ($key + 1) ?></td>
+			<td class="name"><a href="javascript:change('<?php echo $result -> type_name?>','<?php echo $result -> type_id?>');" title="点击修改博客分类"><?php echo $result -> type_name?></a></td>
+			<td class="num"><?php echo $result -> num?></td>
+			<td class="opts">
+				<a href="javascript:change('<?php echo $result -> type_name?>','<?php echo $result -> type_id?>');" title="点击修改博客分类">修改</a>
+				<a href="javascript:deleteType(<?php echo $result -> type_id?>)">删除</a>
+			</td>
+		</tr>
+	<?php
+		}
+	?>
 </tbody></table>
 </form>
 </div>
@@ -89,4 +78,69 @@ include 'admin_check.php';
 	<div class="clear"></div>
 	<div id="OSC_Footer">© 唯创网讯</div>
 </div>
+<script src="js/jquery.min.js"></script>
+<script>
+	function change(typeName,id){
+		$('#txt_link_name').val(typeName).attr('alt',id);
+		$('#cansel-change').css('display','inline');
+		$('#toggle-change').val('修改 »');
+	}
+	$('#toggle-change').on('click',function(){
+		if($('#toggle-change').val()=='修改 »'){
+			changeName()
+		}else if($('#toggle-change').val()=='添加 »'){
+			addType();
+		}
+	});
+	$('#cansel-change').on('click',function(){
+		$('#toggle-change').val('添加 »');
+		$('#cansel-change').css('display','none');
+		$('#txt_link_name').val('');
+	})
+	function changeName(){
+		if(confirm("是否确定修改文章类型名？")){
+			$.post('admin/update_blog_type',{
+				'type_id':$('#txt_link_name').attr('alt'),
+				'type_name':$('#txt_link_name').val()
+			},function(res){
+				if(res == 'success'){
+					alert('修改成功！');
+					location.href = 'admin/classification';
+				}else{
+					alert('修改失败！');
+				}
+
+			},'text')
+		}
+	}
+	function addType(){
+		if(confirm("是否确认添加新的文章类型？")){
+			$.post('admin/save_blog_type',{
+				'type_name':$('#txt_link_name').val()
+			},function(res){
+				if(res == 'success'){
+					alert('添加成功！');
+					location.href = 'admin/classification';
+				}else{
+					alert('添加失败！');
+				}
+
+			},'text');
+		}
+	}
+	function deleteType(id){
+		if(confirm("是否确认删除此文章类型？")){
+			$.post('admin/delete_blog_type',{
+				'type_id':id
+			},function(res){
+				if(res == 'success'){
+					alert('删除成功！');
+					location.href = 'admin/classification';
+				}else{
+					alert('删除失败！');
+				}
+			})
+		}
+	}
+</script>
 </body></html>
